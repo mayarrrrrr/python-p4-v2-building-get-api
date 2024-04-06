af2a50c5690e181c984c95fa5cc4fe01f3913c61
mayarakok <akokmayar607@gmail.com>
2024-04-07 02:22:32 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -20,7 +20,48 @@ def index():
     return "Index for Game/Review/User API"
 
 # start building your API here
+@app.route("/games")
+def games():
+    
+    games = [game.to_dict() for game in Game.query.all()]
+    # for game in Game.query.all():
+    #     game_dict = game.to_dict()
+    #     games.append(game_dict)
+    response = make_response(
+        games,
+        200,
+        {"Content-Type": "application/json"})
+    
+    return response    
 
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    
+    game_dict = game.to_dict()
+    
+    response = make_response(
+        game_dict,
+        200
+    )
+    
+    return response
+
+@app.route("/games/user/<int:id>")
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    users = []
+    
+    for review in game.user:
+        user = review.user
+        user_dict = user.to_dict(rules=("-reviews"))
+        users.append(user_dict)
+        
+    response = make_response(
+        users,
+        200
+    )
+    return response    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
